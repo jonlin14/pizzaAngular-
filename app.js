9766d0bc1da1f65b3d367a4cd2017db29b44a9b3
Jonathan Lin <johnlin0214@gmail.com>
2015-04-22 22:38:34 -0400

    Add controller and factory for pizza and partial for home, toppings, and getStarted

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var myApp = angular.module('myApp', ['ui.router']);
 myApp.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: '',
-    templateUrl: "partials/home.html"
+    templateUrl: 'partials/home.html'
+  });
+  $stateProvider.state('getStarted', {
+    url:'/getStarted',
+    templateUrl: 'partials/getStarted.html',
+    controller: 'pizzaCtrl'
+  });
+  $stateProvider.state('toppings', {
+    url:'/:name',
+    templateUrl: "partials/toppings.html",
+    controller: 'pizzaCtrl'
   });
 });
---
+++ controllers/pizzaCtrl.js
@@ -0,0 +1,3 @@
+myApp.controller('pizzaCtrl', function pizzaCtrl($scope) {
+  $scope.dough = [{ name: "Regular"}, { name: "Flat Bread" }]
+})
